a157665105394fbdc0c12b29b469e69391698374
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2023-01-20 12:21:46 -0300

    Muda app js para funcao

--- src/App.js
+++ src/App.js
@@ -1,73 +1,66 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Switch, Route } from "react-router-dom";
-import "./styles/styles.scss";
 import Home from "./components/Home/Home";
+import "resources/style/style";
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      alterarFonte:
-        (localStorage.getItem("alterarFonte") &&
-          localStorage.getItem("alterarFonte") === "true") ||
-        false,
-      alterarContraste:
-        (localStorage.getItem("alterarContraste") &&
-          localStorage.getItem("alterarContraste") === "true") ||
-        false,
-      focusBuscaAtributo: false
-    };
-    this.alterarFonte = this.alterarFonte.bind(this);
-    this.alterarContraste = this.alterarContraste.bind(this);
-    this.focusBusca = this.focusBusca.bind(this);
+export default function App(props){
+  const initialState = {
+    alterarFonte:
+      (localStorage.getItem("alterarFonte") &&
+        localStorage.getItem("alterarFonte") === "true") ||
+      false,
+    alterarContraste:
+      (localStorage.getItem("alterarContraste") &&
+        localStorage.getItem("alterarContraste") === "true") ||
+      false,
+    focusBuscaAtributo: false,
+    focusBusca: false
+  };
+  const [state, setState] = useState(initialState);
+
+  function handleFocusBusca() {
+    setState({ focusBuscaAtributo: true });
   }
 
-  focusBusca() {
-    this.setState({ focusBuscaAtributo: true });
-  }
-
-  alterarFonte() {
+  function handleAlterarFonte() {
     const alterarFonte =
       localStorage.getItem("alterarFonte") !== null
         ? localStorage.getItem("alterarFonte") !== "true"
         : true;
     localStorage.setItem("alterarFonte", alterarFonte);
-    this.setState({ alterarFonte });
+    setState({ alterarFonte });
   }
 
-  alterarContraste() {
+  function handleAlterarContraste() {
     const alterarContraste =
       localStorage.getItem("alterarContraste") !== null
         ? localStorage.getItem("alterarContraste") !== "true"
         : true;
     localStorage.setItem("alterarContraste", alterarContraste);
-    this.setState({ alterarContraste });
+    setState({ alterarContraste });
   }
 
-  render() {
-    const { alterarFonte, alterarContraste, focusBuscaAtributo } = this.state;
-    return (
-      <div
-        className={`${alterarFonte && "fonte-maior"}
-          ${alterarContraste && "alto-contraste"}`}
-      >
-        <Switch>
-          <Route
-            path="/"
-            exact
-            render={props => (
-              <Home
-                {...props}
-                alterarFonte={this.alterarFonte}
-                alterarContraste={this.alterarContraste}
-                focusBusca={this.focusBusca}
-                focusBuscaAtributo={focusBuscaAtributo}
-                esconderLinkBuscaEscola
-              />
-            )}
-          />
-        </Switch>
-      </div>
-    );
-  }
+
+  return (
+    <div
+      className={`${state.alterarFonte && "fonte-maior"}
+        ${state.alterarContraste && "alto-contraste"}`}
+    >
+      <Switch>
+        <Route
+          path="/"
+          exact
+          render={props => (
+            <Home
+              {...props}
+              alterarFonte={handleAlterarFonte}
+              alterarContraste={handleAlterarContraste}
+              focusBusca={handleFocusBusca}
+              focusBuscaAtributo={state.focusBuscaAtributo}
+            />
+          )}
+        />
+      </Switch>
+    </div>
+  );
 }
