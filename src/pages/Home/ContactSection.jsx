d24d512c80e0929b30720ccb3763c76164a17e64
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2023-01-24 17:38:20 -0300

    Implementa infra http para envio dos dados do formulario para rota post

---
+++ src/pages/Home/ContactSection.jsx
@@ -0,0 +1,108 @@
+import React, {useState} from "react";
+
+import Button from "components/shared/button/Button";
+import InputText from "components/shared/input/InputText";
+import Section from "components/shared/section/Section";
+import { fetchNewContactMessage } from "services/contact.service";
+import Spinner from "components/shared/spinner/Spinner";
+import feedbackService from "application/service/feedbackService";
+
+const defaultState = {
+  name: '',
+  email: '',
+  phone: '',
+  coordenadoria: '',
+  message: ''
+};
+
+export default function ContactSection(){
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState(defaultState);
+
+  function handleChange(ev){
+    setForm(prev => {return {...prev, [ev.target.name]: ev.target.value}})
+  };
+
+  function canBeSubmitted(){
+    return (
+      form.name !== '' &&
+      form.email !== '' &&
+      form.message !== ''
+    );
+  };
+
+  function handleCleanForm(){
+    setForm(defaultState);
+  };
+
+  async function handleSubmit(){
+    setLoading(true);
+    try{
+      await fetchNewContactMessage({
+        ...form,
+        phone : form.phone.replace(/[^\d]/g, '')
+      });
+      handleCleanForm();
+      feedbackService.showSuccessMessage("Mensagem enviada com sucesso. Em breve, COTIC-DISIS entrará em contato.")
+    } catch (err) {
+      feedbackService.showErrorMessage("Ops! Houve um problema ao enviar formulário. Tente novamente mais tarde.")
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Section bgColor="#F8F8F9">
+      <form className="d-flex justify-content-center">
+        <div className="row w-100 w-lg-50 col-md-12">
+          <h2 className="text-center fw-bold mb-4 px-0">
+            Ficou com dúvidas? <span className="text-primary">Escreva</span> para nós!
+          </h2>
+          <InputText
+            label='Nome: *'
+            name='name'
+            value={form.name}
+            callbackChange={handleChange}
+          />
+          <InputText
+            label='E-mail: *'
+            name='email'
+            value={form.email}
+            callbackChange={handleChange}
+          />
+          <div className="row d-flex align-items-end m-0">
+            <div className="col-6 pe-2 ps-0">
+              <InputText
+                label='Telefone:'
+                name='phone'
+                mask='phone'
+                value={form.phone}
+                callbackChange={handleChange}
+              />
+              </div>
+              <div className="col-6 ps-2 pe-0">
+                <InputText
+                  label='Coordenadoria ou Núcleo:'
+                  name='coordenadoria'
+                  value={form.coordenadoria}
+                  callbackChange={handleChange}
+                />
+            </div>
+          </div>
+          <InputText
+            label='Mensagem: *'
+            name='message'
+            value={form.message}
+            callbackChange={handleChange}
+            textarea
+          />
+          <Button
+            title={loading ? <Spinner/> : 'Enviar mensagem'}
+            onClick={handleSubmit}
+            disabled={!canBeSubmitted()}
+          />
+        </div>
+      </form>
+    </Section>
+  );
+};
