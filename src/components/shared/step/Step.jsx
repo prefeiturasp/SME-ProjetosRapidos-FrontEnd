3a458402aa9d094038df2fb243449a76a69e338c
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2023-01-31 17:09:40 -0300

    Melhorias componente step

--- src/components/shared/step/Step.jsx
+++ src/components/shared/step/Step.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import Icon from "../icon/Icon";
 import "./step.scss";
 
-export default function Step({ steps = [] }) {
-  const renderStepItem = ({ title, current = false }) => {
+export default function Step({ steps = [], currentStep }) {
+  const renderStepItem = ({ title, key }) => {
     return (
-      <>
+      <React.Fragment key={key}>
         <div className="text-center">
-          <Icon name={current ? "check-circle" : "circle"} />
+          {key < currentStep ? (
+            <Icon name="check-circle" color="green" />
+          ) : (
+            <span className="text-default">{key}</span>
+          )}
+
           <h6 className="w-100 mt-2 fw-bold">{title}</h6>
         </div>
         <hr width="100%" />
-      </>
+      </React.Fragment>
     );
   };
   return (
